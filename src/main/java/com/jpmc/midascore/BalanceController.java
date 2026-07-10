ec0e6e5652e78a20a8117729385d30392736e41a
Kylan <aliciakimani.ky@gmail.com>
2026-07-10 12:51:20 +0300

    Add REST endpoint for querying user balances
    - Add BalanceController exposing GET /balance?userId= endpoint
    - Return 0 balance for unknown users
    - Configure server.port=33400 so Midas Core serves both
      the Kafka listener and the balance API

---
+++ src/main/java/com/jpmc/midascore/BalanceController.java
@@ -0,0 +1,25 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.component.DatabaseConduit;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private DatabaseConduit databaseConduit;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam long userId) {
+        UserRecord user = databaseConduit.findUserById(userId);
+        if (user == null) {
+            return new Balance(0);
+        }
+        return new Balance(user.getBalance());
+    }
+}
